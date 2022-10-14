f9291c25c9426b4143faf089638f092711e49245
San'Quan Prioleau <sq.prioleau@gmail.com>
2022-10-14 07:13:11 -0400

    ✨ Add skeleton for DailyChallenges component

--- src/components/DailyChallenges/index.tsx
+++ src/components/DailyChallenges/index.tsx
@@ -1,16 +1,21 @@
 import { BackButton, Layout, WelcomeToolbar } from "@/components";
+import dayjs from "dayjs";
 
 export default function DailyChallenges() {
+  const month = dayjs().format("MMMM YYYY");
+
   return (
     <Layout
       headerContent={{
         left: <BackButton />,
-        center: <h1>Daily Challenges</h1>,
+        center: <h1>{month}</h1>,
       }}
       footerContent={<WelcomeToolbar />}
       parentClassName="daily-challenges"
     >
+      <div className="daily-challenges__image"></div>
       Here are your daily challenges
+      <button className="daily-challenges__play-button rounded-full">Play</button>
     </Layout>
   );
 }
