0cb8092fcebbc5d52c77734d304dba29bcb134f1
San'Quan Prioleau <sq.prioleau@gmail.com>
2022-10-15 12:15:33 -0400

    ✨ Add logic to enable changing calendar date

--- src/components/DailyChallenges/index.tsx
+++ src/components/DailyChallenges/index.tsx
@@ -1,24 +1,90 @@
 import { BackButton, Layout, WelcomeToolbar } from "@/components";
 import dayjs from "dayjs";
 import { DailyChallengesCalendar } from "@/components";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useStore, { EDifficulty } from "@/store";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+
+dayjs.extend(advancedFormat);
 
 export default function DailyChallenges() {
-  const month = dayjs().format("MMMM YYYY");
+  const [selectedDate, setSelectedDate] = useState(dayjs());
+  const resetGame = useStore((s) => s.resetGame);
+  const createBoard = useStore((s) => s.createBoard);
+  const navigate = useNavigate();
+
+  // prettier-ignore
+  const handleSelectDate = (dayOfMonth: number) => {
+    const newSelectedDate = selectedDate.date(dayOfMonth);
+    console.log('dayOfMonth:', {dayOfMonth, newSelectedDate: newSelectedDate.format('MMMM Do')});
+    setSelectedDate(newSelectedDate);
+  };
+
+  // prettier-ignore
+  const handleAdvanceMonth = (direction: -1 | 1) => {
+    let newSelectedDate = selectedDate;
+    if (direction === -1) newSelectedDate = dayjs(selectedDate).subtract(1, "month");
+    if (direction === 1) newSelectedDate = dayjs(selectedDate).add(1, "month");
+    console.log("handleAdvanceMonth newSelectedDate:", newSelectedDate.format("MMMM Do"));
+    setSelectedDate(newSelectedDate);
+  };
+
+  const handleStartNewGame = () => {
+    resetGame();
+    createBoard(EDifficulty.easy, () => navigate("/game"));
+  };
+
+  const hues = [233, 273, 333, 22];
+  const hue = hues[selectedDate.month() % hues.length];
 
   return (
     <Layout
       headerContent={{
         left: <BackButton />,
-        center: <h1>{month}</h1>,
+        center: <h1>{selectedDate.format("MMMM YYYY")}</h1>,
       }}
       footerContent={<WelcomeToolbar />}
       parentClassName="daily-challenges"
     >
-      <div className="daily-challenges__image"></div>
-      <div className="daily-challenges__calendar">
-        <DailyChallengesCalendar />
+      <div
+        className="daily-challenges__image"
+        style={{
+          background: `radial-gradient(
+            hsl(${hue}, 100%, 65%), 
+            hsl(${hue + 10}, 100%, 45%)
+          )`,
+        }}
+      >
+        <img
+          src="images/logo_outline.svg"
+          alt="kudosu logo outlined"
+          className="daily-challenges__logo-outlined"
+        />
+        <img
+          src="images/rays_1.svg"
+          alt="conical rays"
+          className="daily-challenges__rays rays-1"
+        />
+        <img
+          src="images/rays_2.svg"
+          alt="conical rays"
+          className="daily-challenges__rays rays-2"
+        />
       </div>
-      <button className="daily-challenges__play-button rounded-full">Play</button>
+      <div className="daily-challenges__calendar">
+        <DailyChallengesCalendar
+          selectedDate={selectedDate}
+          onDateSelect={handleSelectDate}
+          onAdvanceMonth={handleAdvanceMonth}
+        />
+      </div>
+      <button
+        className="daily-challenges__play-button rounded-full"
+        onClick={handleStartNewGame}
+      >
+        Play {selectedDate.format("MMMM Do")}
+      </button>
     </Layout>
   );
 }
--- src/components/DailyChallengesCalendar/index.tsx
+++ src/components/DailyChallengesCalendar/index.tsx
@@ -1,8 +1,60 @@
 import dayjs from "dayjs";
+import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
+import { IconButton } from "@/components";
 
-export default function DailyChallengesCalendar() {
-  const month = dayjs().format("MMMM YYYY");
-  console.log("month:", month);
+interface IProps {
+  selectedDate: dayjs.Dayjs;
+  onDateSelect: (date: number) => void;
+  onAdvanceMonth: (direction: -1 | 1) => void;
+}
 
-  return <div className="daily-challenges-calendar">Calendar</div>;
+export default function DailyChallengesCalendar({
+  selectedDate,
+  onDateSelect,
+  onAdvanceMonth,
+}: IProps) {
+  const currentDate = dayjs();
+  const daysInMonth = selectedDate.daysInMonth();
+  // console.log("Calendar selectedDate:", selectedDate.format("MMMM d"));
+  const dates = [...Array.from({ length: daysInMonth }).keys()].map((index) => index + 1);
+  const firstDayOfMonthIndex = selectedDate.startOf("month").format("d");
+
+  return (
+    <div className="daily-challenges-calendar">
+      <div className="daily-challenges-calendar__nav">
+        <div className="daily-challenges-calendar__nav-button-wrapper prev">
+          <IconButton
+            icon={<RiArrowLeftSLine />}
+            onClick={() => onAdvanceMonth(-1)}
+          />
+        </div>
+        <div className="daily-challenges-calendar__nav-button-wrapper next">
+          <IconButton
+            icon={<RiArrowRightSLine />}
+            onClick={() => onAdvanceMonth(1)}
+          />
+        </div>
+      </div>
+      <div className="daily-challenges-calendar__calendar">
+        <div
+          className="daily-challenges-calendar__spacer"
+          style={{
+            gridColumn: `span ${firstDayOfMonthIndex}`,
+          }}
+        />
+
+        {dates.map((dayOfMonth) => (
+          <button
+            key={dayOfMonth}
+            disabled={dayOfMonth > currentDate.date()}
+            onClick={() => onDateSelect(dayOfMonth)}
+            className="daily-challenges-calendar__date-button rounded-full"
+            data-is-selected={dayOfMonth === selectedDate.date()}
+          >
+            {dayOfMonth}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
