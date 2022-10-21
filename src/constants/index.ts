9b98583b4083d58b427ad3612de482255e3e4b42
San'Quan Prioleau <sq.prioleau@gmail.com>
2022-10-21 11:42:53 -0400

    ♻️ Store storage keys as a constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,13 @@
 export const SUDOKU_PUZZLE_SIZE = 9;
+
+export const DAYS_IN_YEAR = 365;
+
+export const STORAGE_KEYS = {
+  LOCAL_STORAGE: {
+    CURRENT_GAME_STATE: "current_game_state",
+  },
+  INDEXEDDB: {
+    NAME: "kudosu",
+    STORAGE_NAME: "games_played",
+  }
+}
