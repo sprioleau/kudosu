bc4f2e0a1fe8f3f03cbc8fb1b162f3466241dc58
San'Quan Prioleau <sq.prioleau@gmail.com>
2022-10-21 11:00:57 -0400

    🔨 Add script for generating puzzles by day of year

---
+++ scripts/generate-puzzles-by-day.ts
@@ -0,0 +1,26 @@
+import { getSudoku } from "sudoku-gen";
+import { writeFileSync } from "fs"
+
+const DAYS_IN_YEAR = 365;
+
+function generatePuzzlesByDate() { 
+  const puzzles: Record<string, string> = {};
+
+  Array.from(Array(DAYS_IN_YEAR)).forEach((_, index) => {
+    const difficulties = ['easy', 'medium', 'hard', 'expert'];
+    const randomDifficulty = difficulties[Math.floor(Math.random() * difficulties.length)];
+    
+    const { puzzle, solution, difficulty } = getSudoku(randomDifficulty);
+    
+    puzzles[index] = `${difficulties.indexOf(difficulty)}${puzzle.replace(/-/g, "0")}${solution}`;
+  });
+  
+  try {
+    writeFileSync('src/constants/puzzlesByDay.json', JSON.stringify(puzzles), { flag: 'w+' });
+    console.log(puzzles);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+generatePuzzlesByDate();
