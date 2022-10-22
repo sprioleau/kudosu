2c53ffa65b0439e997019458b0ea253eb42a7116
San'Quan Prioleau <sq.prioleau@gmail.com>
2022-10-21 21:33:36 -0400

    ✨ Add progress bar and style same

--- src/components/DailyChallengesCalendar/index.tsx
+++ src/components/DailyChallengesCalendar/index.tsx
@@ -1,12 +1,31 @@
 import dayjs from "dayjs";
 import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
 import { IconButton } from "@/components";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
+import localforage from "localforage";
+import { IInitialState } from "@/store";
+import { SUDOKU_PUZZLE_SIZE } from "@/constants";
 
 interface IProps {
   selectedDate: dayjs.Dayjs;
   onDateSelect: (date: number) => void;
   onAdvanceMonth: (direction: -1 | 1) => void;
+}
+
+async function getGameProgressByDayIndex() {
+  const gameProgressByDayIndex: Record<number, number> = {};
+
+  await localforage.iterate((gameState: IInitialState) => {
+    if (!gameState || !gameState.dailyChallengeDayIndex) return;
+
+    const progress =
+      (100 * (SUDOKU_PUZZLE_SIZE - (gameState.remainingNumberOptions?.length ?? 0))) /
+      SUDOKU_PUZZLE_SIZE;
+
+    gameProgressByDayIndex[gameState.dailyChallengeDayIndex] = progress;
+  });
+
+  return gameProgressByDayIndex;
 }
 
 export default function DailyChallengesCalendar({
@@ -18,6 +37,12 @@ export default function DailyChallengesCalendar({
   const daysInMonth = selectedDate.daysInMonth();
   const dates = [...Array.from({ length: daysInMonth }).keys()].map((index) => index + 1);
   const firstDayOfMonthIndex = selectedDate.startOf("month").format("d");
+
+  const [progressByDayIndex, setProgressByDayIndex] = useState<Record<number, number>>();
+
+  useEffect(() => {
+    getGameProgressByDayIndex().then(setProgressByDayIndex);
+  }, []);
 
   // prettier-ignore
   const handleDateSelect = useCallback((dayOfMonth: number) => {
@@ -60,20 +85,44 @@ export default function DailyChallengesCalendar({
             gridColumn: `span ${firstDayOfMonthIndex}`,
           }}
         />
-        {dates.map((dayOfMonth) => (
-          <button
-            key={dayOfMonth}
-            disabled={getShouldDisableDateButton(dayOfMonth)}
-            onClick={handleDateSelect(dayOfMonth)}
-            className={[
-              "daily-challenges-calendar__date-button rounded-full",
-              `${dayOfMonth === selectedDate.date() ? "selected" : ""}`,
-            ].join(" ")}
-            data-is-selected={dayOfMonth === selectedDate.date()}
-          >
-            {dayOfMonth}
-          </button>
-        ))}
+        {dates.map((dayOfMonth) => {
+          const dayOfYear = dayjs(`${selectedDate.format("YYYY-MM")}-${dayOfMonth}`).dayOfYear();
+          const progress = Math.round(progressByDayIndex?.[dayOfYear] ?? 0);
+          const disabled = getShouldDisableDateButton(dayOfMonth);
+          const STROKE_LENGTH = 320;
+          const arcLength = (STROKE_LENGTH * (progress > 0 ? Math.max(progress, 20) : 0)) / 100;
+
+          return (
+            <button
+              key={dayOfMonth}
+              disabled={disabled}
+              onClick={handleDateSelect(dayOfMonth)}
+              className={[
+                "daily-challenges-calendar__date-button rounded-full",
+                `${dayOfMonth === selectedDate.date() ? "selected" : ""}`,
+              ].join(" ")}
+              data-is-selected={dayOfMonth === selectedDate.date()}
+            >
+              <svg
+                className="daily-challenges-calendar__progress"
+                viewBox="0 0 108 108"
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="var(--c-accent-text)"
+                fill="none"
+                strokeWidth={8}
+                strokeOpacity={disabled ? 0 : 1}
+              >
+                <circle
+                  cx="54"
+                  cy="54"
+                  r="50"
+                  strokeDasharray={`${arcLength} 2000`}
+                />
+              </svg>
+              {dayOfMonth}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
